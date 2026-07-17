3c51d421b152fce0e7a69c6b017097d9c31ba80d
ganeshkhairedevops <ganesh.khaire14@gmail.com>
2026-07-17 15:54:40 +0530

    issue

--- devops/agent.py
+++ devops/agent.py
@@ -3,10 +3,26 @@ from langchain.agents import create_agent
 
 from llm import llm
 from prompts import SYSTEM_PROMPT
+from tools.kubernetes import (
+    get_pods,
+    get_nodes,
+    get_services,
+    get_deployments,
+)
 
-from tools.kubernetes import *
-from tools.docker import *
-from tools.linux import *
+from tools.docker import (
+    docker_ps,
+    docker_images,
+)
+
+from tools.linux import (
+    disk_usage,
+    memory_usage,
+)
+
+# from tools.kubernetes import *
+# from tools.docker import *
+# from tools.linux import *
 
 agent = create_agent(
     model=llm,
