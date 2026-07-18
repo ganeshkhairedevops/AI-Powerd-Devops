e9876285fa0f3dbed6fe29297f2e30804c31532d
ganeshkhairedevops <ganesh.khaire14@gmail.com>
2026-07-18 11:37:48 +0530

    update llm for low conf

--- devops/llm.py
+++ devops/llm.py
@@ -2,8 +2,14 @@
 
 from langchain_ollama import ChatOllama
 
+# llm = ChatOllama(
+#     model="qwen3-coder:30b",
+#     temperature=0,
+#     num_ctx=8192,
+# )
+# used for low config systems, like my laptop, and for testing
 llm = ChatOllama(
-    model="qwen3-coder:30b",
+    model="qwen2.5-coder:7b",
     temperature=0,
-    num_ctx=8192,
+    num_ctx=2048,
 )
