e8dbd1b29bc561c593020ce57172f9613ed1230a
ganeshkhairedevops <ganesh.khaire14@gmail.com>
2026-07-18 11:03:35 +0530

    add docker tools

--- devops/tools/docker.py
+++ devops/tools/docker.py
@@ -24,3 +24,33 @@ def docker_networks():
 def docker_volumes():
     """List Docker volumes."""
     return run_command(["docker", "volume", "ls"])
+
+@tool
+def docker_version():
+    """Show the Docker version."""
+    return run_command(["docker", "version"])
+
+@tool
+def docker_ps_all():
+    """List all Docker containers."""
+    return run_command(["docker", "ps", "-a"])
+
+@tool
+def docker_logs(container: str):
+    """Show logs for a Docker container."""
+    return run_command(["docker", "logs", container])
+
+@tool
+def docker_inspect(container: str):
+    """Inspect a Docker container."""
+    return run_command(["docker", "inspect", container])
+
+@tool
+def docker_image_inspect(image: str):
+    """Inspect a Docker image."""
+    return run_command(["docker", "image", "inspect", image])
+
+@tool
+def docker_exec(container: str, command: str):
+    """Run a command inside a running container."""
+    return run_command(["docker", "exec", container, "sh", "-c", command])
