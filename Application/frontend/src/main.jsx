22dcbe4638e0f1bbdfe9471847e0e1d33594093a
ganeshkhairedevops <ganesh.khaire14@gmail.com>
2026-07-28 17:39:33 +0530

    main.jsx

--- Application/frontend/src/main.jsx
+++ Application/frontend/src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import "./index.css";
 import App from "./App";
 
+import { ChatProvider } from "./context/ChatContext";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ChatProvider>
+      <App />
+    </ChatProvider>
   </React.StrictMode>
 );
