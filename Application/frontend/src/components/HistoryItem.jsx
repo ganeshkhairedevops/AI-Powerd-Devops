d49b01c1f4520a97f399a3d25b789122bbf26d4e
ganeshkhairedevops <ganesh.khaire14@gmail.com>
2026-07-29 10:46:58 +0530

    add history item

--- Application/frontend/src/components/HistoryItem.jsx
+++ Application/frontend/src/components/HistoryItem.jsx
@@ -1,19 +1,48 @@
-function HistoryItem({ title, active, onClick }) {
+function HistoryItem({
+
+    title,
+
+    active,
+
+    onClick
+
+}) {
 
     return (
 
         <button
+
             onClick={onClick}
-            className={`w-full text-left p-3 rounded-lg mb-2 transition
+
+            className={`
+
+            w-full
+
+            text-left
+
+            p-3
+
+            rounded-lg
+
+            mb-2
+
+            transition
 
             ${
+
                 active
+
                     ? "bg-cyan-700"
+
                     : "hover:bg-slate-700"
-            }`}
+
+            }
+
+            `}
+
         >
 
-            {title}
+            ?? {title}
 
         </button>
 
